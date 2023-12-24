ab6c74b1ed675c3a88d6e46185cd5fbe865a5197
Alcides Tiago <alcides.tiago@academico.ifrn.edu.br>
2023-12-23 21:26:06 -0300

    inclui redirecionamento endpoint raiz.

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+from fastapi.responses import RedirectResponse
 from database import engine, Base
 from fastapi import FastAPI
 from routers import user, auth
@@ -8,3 +9,9 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(swagger_ui_parameters={"syntaxHighlight.theme": "nord"})
 app.include_router(user.router)
 app.include_router(auth.router)
+
+
+@app.get("/", include_in_schema=False)
+async def redirect():
+    response = RedirectResponse(url="/docs")
+    return response
